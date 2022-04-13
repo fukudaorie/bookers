0332b6d533c9bcad877e63e914382a6ab17ac88b
EC2 Default User <ec2-user@ip-172-31-46-87.ap-northeast-1.compute.internal>
2022-04-13 14:39:24 +0000

    3-12-indexページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,29 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end  
   end
 
   def show
+    @list = List.find(params[:id]) 
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
